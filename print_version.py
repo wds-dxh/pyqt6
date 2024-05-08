c8b5a9902477b6b300a3276a4009af6c51671c2f
wds-dxh <wdsnpshy@163.com>
2024-05-08 10:58:08 +0800

    test-v2ray

---
+++ print_version.py
@@ -0,0 +1,5 @@
+from PyQt6.QtCore import QT_VERSION_STR   #(清华源)#pip install PyQt6 -i https://pypi.tuna.tsinghua.edu.cn/simple
+from PyQt6.QtCore import PYQT_VERSION_STR
+
+print(QT_VERSION_STR)
+print(PYQT_VERSION_STR)
